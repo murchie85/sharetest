667f96e7660e7f5ffd242256dc9c9bf3250b782c
murchie85 <murchie85@gmail.com>
2025-01-24 14:49:59 +0000

    more

--- parsing.cs
+++ parsing.cs
@@ -4,3 +4,17 @@ var filtered = documents.EnumerateArray()
        JsonDocument.Parse(doc.GetProperty("Properties").GetString())
            .RootElement.GetProperty("PluginVersion").GetString() == "10"
    );
+
+
+
+
+
+var filtered = resultDoc.EnumerateArray()
+    .Where(doc => doc.GetProperty("clientMachine").GetString() == CertificateStore.ClientMachine);
+
+LogHandlerCommon.Info(logger, CertificateStore, $"Found matches: {filtered.Count()}");
+
+foreach (var doc in filtered)
+{
+    LogHandlerCommon.Info(logger, CertificateStore, $"Match found: {doc}");
+}
